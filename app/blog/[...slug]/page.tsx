40b28d3670eddbb45604246898aad07576545ed2
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2024-05-14 16:33:40 +0100

    setup post page

---
+++ app/blog/[...slug]/page.tsx
@@ -0,0 +1,41 @@
+import { posts } from "#site/content";
+import { MDXContent } from "@/components/MDXComponents";
+import { notFound } from "next/navigation";
+interface PostPageProps {
+	params: {
+		slug: string[];
+	};
+}
+
+async function getPostFromParmas(params: PostPageProps["params"]) {
+	const slug = params?.slug.join("/");
+	const post = posts.find((post) => post.slugAsParams === slug);
+	return post;
+}
+
+export async function getStaticParams(): Promise<PostPageProps["params"][]> {
+	return posts.map((post) => ({ slug: post.slugAsParams.split("/") }));
+}
+
+const Post = async ({ params: { slug } }: PostPageProps) => {
+	const post = await getPostFromParmas({ slug });
+
+	if (!post || !post.published) {
+		notFound();
+	}
+
+	return (
+		<article className="contaienr py-6 prose dark:prose-invert max-w-3xl mx-auto">
+			<h1 className="mb-2">{post.title}</h1>
+			{post.description ? (
+				<p className="text-xl mt-0  text-muted-foreground">
+					{post.description}
+				</p>
+			) : null}
+			<hr className="my-4" />
+			<MDXContent code={post.body} />
+		</article>
+	);
+};
+
+export default Post;
