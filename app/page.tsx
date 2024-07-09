820a16b834b0926ad2eeac4be94bb5d712c96fe1
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2024-07-09 22:19:14 +0100

    feat: Add Contact component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/pages/About";
 import Hero from "@/components/pages/Hero";
+import BlurFade from "@/components/ui/Blur";
+import Contact from "@/components/ui/Contact";
 import Experience from "@/components/ui/Experience";
 import SectionHeader from "@/components/ui/SectionHeader";
 
 export default function Home() {
 	return (
-		<>
+		<BlurFade>
 			<header>
 				<Hero />
 			</header>
@@ -14,7 +16,11 @@ export default function Home() {
 				<About />
 				<SectionHeader title="Experience" direction="row-reverse" />
 				<Experience />
+				<SectionHeader title="Projects" direction="row" />
+
+				<SectionHeader title="Contact" direction="row-reverse" />
+				<Contact />
 			</main>
-		</>
+		</BlurFade>
 	);
 }
---
+++ components/ui/Contact.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+/* eslint-disable react/no-unescaped-entities */
+import { motion } from "framer-motion";
+import Link from "next/link";
+import React from "react";
+
+const Contact = () => {
+	return (
+		<motion.div
+			whileInView={{
+				opacity: 1,
+				transition: { duration: 0.5, ease: "easeOut" },
+			}}
+			initial={{ opacity: 0 }}
+			transition={{ duration: 0.7 }}
+			className="grid place-items-center my-12"
+			viewport={{ once: true, amount: "all" }}
+		>
+			<div className="bg-accent max-sm:w-full w-[500px] h-[300px] rounded-xl flex-col flex  justify-center items-center">
+				<h2 className="text-3xl md:text-5xl font-black text-end">
+					Contact
+					<span className="text-primary">.</span>
+				</h2>
+				<div className="max-w-[400px]">
+					<p className="text-center my-4 text-muted-foreground leading-relaxed">
+						Shoot me an{" "}
+						<Link
+							href={"mailto:stevefernandes24@hotmail.co.uk"}
+							target="_blank"
+							className="text-primary font-bold"
+						>
+							{" "}
+							Email{" "}
+						</Link>{" "}
+						if you want to connect! You can also find me on
+						<Link
+							href={"https://www.linkedin.com/in/stavfernandes24/"}
+							target="_blank"
+							className="text-primary font-bold"
+						>
+							{" "}
+							Linkedin{" "}
+						</Link>
+						if that's more your speed.
+					</p>
+				</div>
+			</div>
+		</motion.div>
+	);
+};
+
+export default Contact;
