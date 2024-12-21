e97c6d598328710f18bb342e5b53d46fea3ca2b8
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2024-12-21 09:09:09 +0000

    Refactor: Update YesNo component styles

--- components/ui/YesNo.tsx
+++ components/ui/YesNo.tsx
@@ -10,11 +10,11 @@ type YesNoProps = {
 const YesNo = ({ type, children }: YesNoProps) => {
 	const className =
 		type === "yes"
-			? "text-white border rounded-lg px-4 py-2 border-green-500 bg-green-500/10 my-4"
-			: "text-white border rounded-lg px-4 py-2 border-red-500 bg-red-500/10 my-4";
+			? "text-black dark:text-white/90 border rounded-lg px-4 py-2 border-green-500 bg-green-500/10 my-4"
+			: "text-black dark:text-white/90  border rounded-lg px-4 py-2 border-red-500 bg-red-500/10 my-4";
 	return (
 		<div className={className}>
-			<div className=" mt-2 w-10 h-10 rounded-full bg-white/10 flex items-center justify-center">
+			<div className=" mt-2 w-10 h-10 rounded-full bg-white/10 flex items-center justify-center   ">
 				{type === "yes" ? (
 					<CheckIcon className="text-green-500" />
 				) : (
